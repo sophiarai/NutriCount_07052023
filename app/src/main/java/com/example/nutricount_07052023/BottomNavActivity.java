18236d4226d7b108ce5236c551f9b96a9c867341
bastst <bastian.steinmair@gmail.com>
2023-05-17 12:10:41 +0200

    ...

--- app/src/main/java/com/example/nutricount_07052023/BottomNavActivity.java
+++ app/src/main/java/com/example/nutricount_07052023/BottomNavActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.w3c.dom.Text;
